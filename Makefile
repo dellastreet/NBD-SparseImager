613139ba33ce3237bcac6b0cd6bf91492cad4bb9
Della Street <dellastreet@live.nl>
2010-03-05 04:54:28 -0800

    Some initial web-page

--- Makefile
+++ Makefile
@@ -7,10 +7,17 @@ clean:
 install: all
 	install -d ${DESTDIR}/usr/local/sparse-imager/bin/
 	install -d ${DESTDIR}/usr/share/doc/sparse-imager/
+	install -d ${DESTDIR}/var/www/spim/
+	install -d ${DESTDIR}/var/www/spim/img/
 	
-	install -D -m 500 imagerdb.py ${DESTDIR}/usr/local/sparse-imager/bin/
-	install -D -m 500 broadcast_listener ${DESTDIR}/usr/local/sparse-imager/bin/
-	install -D -m 500 xnbd/trunk/xnbd-bgctl ${DESTDIR}/usr/local/sparse-imager/bin/
-	install -D -m 500 xnbd/trunk/xnbd-server ${DESTDIR}/usr/local/sparse-imager/bin/
+	install -D -m 540 imagerdb.py ${DESTDIR}/usr/local/sparse-imager/bin/
+	install -D -m 540 broadcast_listener ${DESTDIR}/usr/local/sparse-imager/bin/
+	install -D -m 540 xnbd/trunk/xnbd-bgctl ${DESTDIR}/usr/local/sparse-imager/bin/
+	install -D -m 540 xnbd/trunk/xnbd-server ${DESTDIR}/usr/local/sparse-imager/bin/
 	
 	install -D -m 500 dbschema.sql ${DESTDIR}/usr/share/doc/sparse-imager/
+	
+	
+	install -D -o 33 -m 400 www/status.php ${DESTDIR}/var/www/spim/ 
+	install -D -o 33 -m 400 www/db.php ${DESTDIR}/var/www/spim/ 
+	install -D -o 33 -m 400 www/images/DiskTransparantSmall.png ${DESTDIR}/var/www/spim/img/
---
+++ www/status.php
@@ -0,0 +1,153 @@
+<?php ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<style type="text/css">
+body{
+margin: 0;
+background: #557446;
+font-family: verdana, arial, helvetica, sans-serif;
+color: #CFDACA;
+font-size: 80%;
+}
+
+div#container{
+width: 100%;
+height: 100%;
+}
+
+div#logo{
+width: 100px;
+height: 100px;
+background: #CFDACA;
+color: #557446;
+text-align: center;
+padding: 20px 0 0 0;
+float: left;
+clear: right;
+}
+
+div#header{
+width: 100%;
+height: 100px;
+background: #CFDACA;
+color: #557446;
+text-align: center;
+padding: 20px 0 0 0;
+float: top;
+}
+
+div#nav_container{
+width: 100px;
+margin: 50px 20px 0 20px;
+border-top: 1px solid #CFDACA;
+border-left: 1px solid #CFDACA;
+padding: 7px;
+float: left;
+}
+
+div#sub_content{
+width: 200px;
+margin: 50px 20px 0 20px;
+border-top: 1px solid #CFDACA;
+border-left: 1px solid #CFDACA;
+padding: 7px;
+float: right;
+}
+
+div#content{
+margin: 50px 280px 0 180px;
+border-top: 1px solid #CFDACA;
+}
+
+ul{
+list-style: none;
+margin: 0;
+padding: 0;
+}
+
+li{
+margin: 0;
+padding: 0;
+}
+
+a:link, a:visited{
+color: #CFDACA;
+text-decoration: underline;
+font-weight: bold;
+}
+
+a:hover, a:active{
+color: #C53402;
+text-decoration: underline;
+font-weight: bold;
+}
+
+ul a:link, ul a:visited{
+color: #CFDACA;
+text-decoration: none;
+font-weight: normal;
+}
+
+ul a:hover, ul a:active{
+color: #C53402;
+text-decoration: underline;
+font-weight: normal;
+}
+
+h1{
+font-size: 100%;
+margin: 0;
+padding: 10px 0 10px 0;
+}
+
+p{
+margin: 0;
+padding: 0 0 10px 0;
+}
+
+.explain{
+color: #FDAB24;
+}
+</style>
+</head>
+
+<body>
+<div id="container">
+
+<div id="logo"><img src="img/DiskTransparantSmall.png"></div> 
+<div id="header"><p>Forensic Sparse Imager</p>
+</div>
+
+<div id="nav_container">
+<ul>
+<li><a href="#">Status</a></li>
+<li><a href="#">Tasks</a></li>
+</ul>
+</div>
+
+<div id="sub_content">
+<p><span class="caps">NBD</span>SparseImager is a solution for imaging medium quantities of hard-disks for forensic analysis.</p>
+<p>The solution is intended to solve the folowing problems:</p>
+<ul>
+	<li>Disks get bigger all the time</li>
+	<li>Full imaging of several 1T disks becomes inpractical in terms of storage</li>
+
+	<li>Full imaging of several 1T disks becomes inpractical in terms of time</li>
+	<li>Depending on the type of investigation not all data is as relevant (e.g. content of dll&#8217;s and executables in general are not that relevant)</li>
+</ul>
+</div>
+
+<div id="content">
+<h1>Heading</h1>
+
+</div>
+
+</div>
+
+</body>
+</html>
+
+
+
