5f751287127d11386c768bc3cee31d01cd936fb5
Della Street <dellastreet@live.nl>
2010-03-19 14:58:10 -0700

    Thought a bit about a task list and scheduler.
    Some basic prototype datastuctures added. Not even tested if it is correct SQL

--- dbschema.sql
+++ dbschema.sql
@@ -10,3 +10,27 @@ CREATE TABLE systems (
 	seenat DATETIME 
 );
 
+CREATE TABLE defaulttasks (
+        id int PRIMARY KEY ,
+        depends_on int,
+	priority int, 
+	material enum ('disk', 'partition', 'filesystem'),
+        command varchar (256),
+	defaultrun bool
+);
+
+-- priority: lower is better
+
+INSERT into defaulttasks values (1,0,1,partition, "fls -r $PART",true);
+INSERT into defaulttasks values (2,0,1,filesystem, "find $ROOT -iname \*.log -exec cat '{}' \;",true);
+INSERT into defaulttasks values (1,0,1,partition, "find $ROOT -iname \*.evt -exec cat '{}' \;",true);
+
+CREATE TABLE tasks (
+        id int PRIMARY KEY ,
+	systemname varchar (40),
+        defaulttask_id int,
+	state enum { 'idle', 'waiting', 'running' , 'finished'),
+	finished DATETIME
+);
+
+
